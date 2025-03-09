ffe3cea86292ebba57ff824df99908e8f0fafa90
“ZhouhengTao“ <zhouheng0118@gmail.com>
2025-03-08 22:01:45 -0800

    Add the error method.

---
+++ src/ladder.cpp
@@ -0,0 +1,6 @@
+#include "ladder.h"
+
+void error(string word1, string word2, string msg) {
+    cout << "Cannot create word ladder from '" << word1 << "' to '" << word2 << "': " << msg << endl;
+}
+
