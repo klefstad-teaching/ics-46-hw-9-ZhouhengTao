9e693b66bc94222ea613c67f82b91579de11025b
“ZhouhengTao“ <zhouheng0118@gmail.com>
2025-03-14 16:55:11 -0700

    Fix a error in print method in dijkstras.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -49,17 +49,16 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
-        cout << "No path exists" << endl;
+        cout << "\nTotal cost is " << total << endl;
         return;
     }
-    
-    cout << "Path: ";
+
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
         if (i < path.size() - 1) {
-            cout << " -> ";
+            cout << " ";
         }
     }
     
-    cout << ", Total Cost: " << total << endl;
+    cout << " \nTotal cost is " << total << endl;
 }
